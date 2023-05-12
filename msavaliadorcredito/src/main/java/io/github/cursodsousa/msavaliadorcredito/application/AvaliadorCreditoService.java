2a32ef1a24158b58a0bd86c6d0703013db3b1e53
Carlos Henrique <ichfs9108@gmail.com>
2023-05-12 15:07:22 -0300

    Comunicacao sincrona com o OpenFeign

--- msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,17 +1,27 @@
 package io.github.cursodsousa.msavaliadorcredito.application;
 
+import io.github.cursodsousa.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.cursodsousa.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.cursodsousa.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
+    private final ClienteResourceClient clientesClient;
+
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         // obterDadosCliente - MSCLIENTES
         // obter cartoes do cliente - MsCartoes
-        return new SituacaoCliente();
+        ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+
+        return SituacaoCliente
+                .builder()
+                .cliente(dadosClienteResponse.getBody())
+                .build();
     }
 
 }
