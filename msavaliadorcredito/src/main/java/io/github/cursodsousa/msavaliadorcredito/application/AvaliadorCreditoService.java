3609dd3b2e1e0bf83777042b24e3666adfe42358
Carlos Henrique <ichfs9108@gmail.com>
2023-05-13 01:47:01 -0300

    Buscando os dados de cartoes pelo microservice de cartoes

--- msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/cursodsousa/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,26 +1,31 @@
 package io.github.cursodsousa.msavaliadorcredito.application;
 
+import io.github.cursodsousa.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.cursodsousa.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.cursodsousa.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.cursodsousa.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.github.cursodsousa.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesResourceClient;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
-        // obterDadosCliente - MSCLIENTES
-        // obter cartoes do cliente - MsCartoes
         ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesResourceClient.getCartoesByCliente(cpf);
 
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
     }
 
